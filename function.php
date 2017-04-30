ef05441f3351c36ceeacf3ae1908b4f3535a0b3e
steelydylan <ess_president@me.com>
2017-04-30 15:25:25 +0900

    added new

---
+++ function.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: SmartPhoto
+Plugin URI: http://www.example.com/plugin
+Description: テキストを表示するだけのプラグイン
+Author: appleple
+Version: 0.1
+Author URI: http://www.example.com
+*/
+
