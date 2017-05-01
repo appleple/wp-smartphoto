6d33cb34653fb7e0766e12b3c69dd601b92e95c8
steelydylan <ess_president@me.com>
2017-05-01 17:49:31 +0900

    added

--- lib/admin.php
+++ lib/admin.php
@@ -2,13 +2,53 @@
 
 class smartphoto_options {
 	function __construct() {
-		add_action('admin_menu', array($this, 'render'));
+		add_action('admin_menu', array($this, 'add_pages'));
 	}
-  function render() {
-		?>
-			<div>hoge</div>
-		<?php
+  function add_pages() {
+		add_menu_page('SmartPhoto','SmartPhoto', 'level_8', __FILE__, array($this,'show_text_option_page'), '', 26);
   }
+	function get_options() {
+		$options = get_option('smartphoto_options');
+		if (!is_array($options)) {
+			$options['arrows'] = true;
+			$options['nav'] = true;
+			$options['animationSpeed'] = true;
+			$options['swipeOffset'] = 100;
+			$options['forceInterval'] = 10;
+			$options['registance'] = 0.5;
+			$options['resizeStyle'] = 'fill';
+			$options['verticalGravity'] = false;
+			$options['useOrientationApi'] = true;
+			update_option('smartphoto_options', $options);
+		}
+		return $options;
+	}
+	function show_text_option_page() {
+		$opt = $this->get_options();
+		?>
+			<h1>SmartPhoto</h1>
+			<form method="post" action="" enctype="multipart/form-data">
+				<table>
+					<tr>
+						<th>arrows</th>
+						<td></td>
+					<tr>
+						<th>animationSpeed</th>
+						<td><input type="number" name="smartphoto_options[animationSpeed]" value="<?php echo $opt['animationSpeed'] ?>"></td>
+					</tr>
+					<tr>
+						<th>swipeOffset</th>
+						<td><input type="number" name="smartphoto_options[swipeOffset]" value="<?php  echo $opt['swipeOffset'] ?>"></td>
+					</tr>
+					<tr>
+						<th>forceInterval</th>
+						<td><input type="number" name="smartphoto_options[forceInterval]" value="<?php echo $opt['forceInterval'] ?>"></td>
+					</tr>
+				</table>
+				<input type="submit" name="Submit" value="Save" />
+			</form>
+		<?php
+	}
 }
 
-new smartphoto_options
+$options = new smartphoto_options;
--- smartphoto.php
+++ smartphoto.php
@@ -5,15 +5,15 @@ Plugin URI: https://github.com/appleple/wp-smartphoto
 Description: The most easy to use responsive image viwer especially for mobile devices
 Author: appleple
 Version: 0.1.0
-License: MIT
+License: GPL2
 Author URI: https://appleple.github.io/
 */
 
 function add_assets() {
-	wp_register_script('SmartPhoto', 'https://unpkg.com/smartphoto@latest/js/smartphoto.min.js');
-	wp_register_style('SmartPhoto', 'https://unpkg.com/smartphoto@latest/css/smartphoto.min.css');
-	wp_enqueue_script('SmartPhoto');
-	wp_enqueue_style('SmartPhoto');
+	wp_enqueue_script('smartphoto-js','https://unpkg.com/smartphoto@latest/js/smartphoto.min.js');
+	wp_enqueue_style('smartphoto-css','https://unpkg.com/smartphoto@latest/css/smartphoto.min.css');
 }
+
+add_action('wp_enqueue_scripts', 'add_assets');
 
 include_once('lib/admin.php');
